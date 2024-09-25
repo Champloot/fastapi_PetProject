d05b07e8cbd2a7ae639d62e38a90397cab04fb71
Archi <94532438+Champloot@users.noreply.github.com>
2024-09-26 01:09:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,3 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(tasks_router)
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
